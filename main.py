f40fa7ff0aa9c91a2c6e43488e1dba0c3a746e21
lvweiguang <35168719+lvweiguang@users.noreply.github.com>
2023-05-29 17:44:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ class SendMessage():                                                 #定义发�
                         }
         self.appID = 'wx9f7ff34b6704c03a'                             #appid 注册时有
         self.appsecret = 'd24024b5b59620a0f7d14e3f1161d9a7'           #appsecret 同上
-        self.template_id = 'UrykEL3q2yuDan_oeDUb19SjaAvGS_UpiqI9-Qv3P0s'  # 模板id
+        self.template_id = 'xG9cCjgdKBg5cT2XgmCN1nsqzw1anu8LocKQpubbafc'  # 模板id
         self.access_token = self.get_access_token()                   #获取 access token
         self.opend_ids = self.get_openid()                            #获取关注用户的openid
  
